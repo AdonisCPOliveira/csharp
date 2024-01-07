996aeb31f620b5ae2097aa7524bb34a6bf6da9e1
AdonisCPOliveira <estudo.adonis@gmail.com>
2024-01-07 03:13:30 -0300

    Commit Inicial

---
+++ WinFormsApplication/WinFormsApplication/Form1.cs
@@ -0,0 +1,15 @@
+namespace WinFormsApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            txtResultado.Text = "Seu primeiro programa";
+        }
+    }
+}
